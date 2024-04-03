6852453298e3bed18d8ded9598d40d26c75f5412
Kevin Shan <kevinshan2017@gmail.com>
2024-04-03 09:11:43 -0400

    inital class

--- FoodPrice.java
+++ FoodPrice.java
@@ -1,3 +1,15 @@
 public class FoodPrice {
-    
+    String date;
+    String admin1;
+    String admin2;
+    String market;
+    double latitude;
+    double longitude;
+    String category;
+    String commodity;
+    String unitpriceflag;
+    String pricetype;
+    String currency;
+    double price;
+    double usdprice;
 }
